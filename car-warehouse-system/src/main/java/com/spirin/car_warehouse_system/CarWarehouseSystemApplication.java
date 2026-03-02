f56c990a1159eae8a0fc1f32ea5fddf840f9b678
Mikhal Spirin <spirin_0505@mail.ru>
2026-03-02 20:34:44 +0300

    Add Spring Boot project

---
+++ car-warehouse-system/src/main/java/com/spirin/car_warehouse_system/CarWarehouseSystemApplication.java
@@ -0,0 +1,13 @@
+package com.spirin.car_warehouse_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarWarehouseSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarWarehouseSystemApplication.class, args);
+	}
+
+}
---
+++ car-warehouse-system/src/test/java/com/spirin/car_warehouse_system/CarWarehouseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spirin.car_warehouse_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarWarehouseSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
